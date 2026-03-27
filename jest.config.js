1c8563dea92b13fc92405e6f562a335bbc4a81e2
B Ashok <batakala.ashok@netradyne.com>
2026-03-27 21:15:39 +0530

    Mega cleanup

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  testEnvironment: "jest-environment-jsdom",
-  transform: {},
-  setupFilesAfterEnv: ["<rootDir>/tests/setup.js"],
-  testMatch: ["<rootDir>/tests/**/*.test.js"],
-};
